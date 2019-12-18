611f1cadd30e3814c5f26b461f6efb5c9217e114
Darlysson <felipetads2012@gmail.com>
2019-12-18 17:53:23 +0000

    leadspace component updated

--- src/components/Leadspace/index.js
+++ src/components/Leadspace/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import styles from './style.module.css';
-import { GiStrong, GiArrowhead } from 'react-icons/gi';
 import { MdKeyboardArrowRight } from 'react-icons/md';
+
+import strong from './img/strong.png'
+import arrow from './img/arrow.png'
 
 export default () => (
   <div className={styles.leadspaceContainer}>
     <h1>
-      Power to <GiStrong /><br />
-      <GiArrowhead /> founders
+      <span>Power to <img src={strong} className={styles.arm} alt="arm"/></span> 
+      <img src={arrow} className={styles.arrow} alt="Arrow"/> founders
     </h1>
     <h2>We run programs to power the growth <br />of startups we invest in.</h2>
     <button>Work with us <MdKeyboardArrowRight /></button>
